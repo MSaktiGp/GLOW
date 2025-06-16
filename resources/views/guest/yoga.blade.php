44fc1c5d06ac4bfcfceae89fae2e0a9e1c655fd7
shakila10 <shakila.rama10@gmail.com>
2025-06-16 18:33:37 +0700

    tmbh y

--- resources/views/guest/yoga.blade.php
+++ resources/views/guest/yoga.blade.php
@@ -20,7 +20,7 @@
     </section>
 
     <!-- Coaches -->
-<<<<<<< HEAD
+
    <section class="max-w-6xl mx-auto px-4 pb-10">
     <h2 class="text-[#F189B8] font-bold text-xl mb-6">OUR BEST COACH</h2>
     <div class="grid md:grid-cols-3 gap-6">
@@ -37,7 +37,7 @@
                 class="mt-2 inline-block bg-[#F189B8] text-white px-4 py-2 rounded-full hover:bg-pink-600">Book Now</a>
             </div>
         @endforeach
-=======
+    </div>
     <section class="max-w-6xl mx-auto px-4 pb-10">
         <h2 class="text-[#F189B8] font-bold text-xl mb-6">OUR BEST COACH</h2>
         <div class="grid md:grid-cols-3 gap-6">
